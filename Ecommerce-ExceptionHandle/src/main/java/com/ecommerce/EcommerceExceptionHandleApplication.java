569546bbef2f2e183a43b025a897d4db455d934e
iPriyanshuRanjan <71120211+iPriyanshuRanjan@users.noreply.github.com>
2023-09-12 12:10:40 +0530

    Exception

---
+++ Ecommerce-ExceptionHandle/src/main/java/com/ecommerce/EcommerceExceptionHandleApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceExceptionHandleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceExceptionHandleApplication.class, args);
+	}
+
+}
---
+++ Ecommerce-ExceptionHandle/src/main/java/com/ecommerce/entity/EProduct.java
@@ -0,0 +1,39 @@
+package com.ecommerce.entity;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+
+public class EProduct {
+	private long id;
+	private String name;
+	private BigDecimal price;
+	private Date dateAdded;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public BigDecimal getPrice() {
+		return price;
+	}
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	public Date getDateAdded() {
+		return dateAdded;
+	}
+	public void setDateAdded(Date dateAdded) {
+		this.dateAdded = dateAdded;
+	}
+	
+	
+
+}
